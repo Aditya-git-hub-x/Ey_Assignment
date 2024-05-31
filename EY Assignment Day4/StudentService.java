7a530f84a4adec3794c537a9a8d2668b6b5a758b
Aditya kumar <78842477+Aditya-git-hub-x@users.noreply.github.com>
2024-05-31 17:08:00 +0530

    Add files via upload

---
+++ EY Assignment Day4/StudentService.java
@@ -0,0 +1,13 @@
+package com.ey.oops.task;
+import java.util.Date;
+
+public class StudentService {
+	
+	public void ShowStudentDetails(Student []s) {
+		for(int i=0;i<s.length;i++) {
+			System.out.println(s[i]);
+		}
+	}
+
+
+}
