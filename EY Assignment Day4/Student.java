403b3781409ba5189998644300fa1d6f75f2ec0d
Aditya kumar <78842477+Aditya-git-hub-x@users.noreply.github.com>
2024-05-31 18:37:15 +0530

    Add files via upload

---
+++ EY Assignment Day4/Student.java
@@ -0,0 +1,111 @@
+package com.ey.oops.task;
+
+import java.util.Arrays;
+import java.util.Date;
+
+public class Student {
+	
+	private Integer admissionCode;
+	private String studentName;
+	private Date birthdate;
+	private Integer []marks;
+	private String grade;
+	private static Integer admissionCounter;
+	private static String SchoolName;
+	
+	static {
+		SchoolName="Hyderabad Public School";
+	}
+	
+	public Student() {
+		this.admissionCode = admissionCounter + 100;
+	}
+	
+	public Student(String studentName, Date birthdate) {
+		
+		this();
+		this.studentName = studentName;
+		this.birthdate = birthdate;
+	}
+	
+	public Student(String studentName, Date birthdate, Integer[] marks) {
+		this(studentName, birthdate);
+		this.marks=marks;
+	}
+	
+	
+
+
+	public Integer getAdmissionCode() {
+		return admissionCode;
+	}
+
+	public void setAdmissionCode(Integer admissionCode) {
+		this.admissionCode = admissionCode;
+	}
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+	public Date getBirthdate() {
+		return birthdate;
+	}
+
+	public void setBirthdate(Date birthdate) {
+		this.birthdate = birthdate;
+	}
+
+	public Integer[] getMarks() {
+		return marks;
+	}
+
+	public void setMarks(Integer[] marks) {
+		this.marks = marks;
+	}
+
+	public String getGrade() {
+		return grade;
+	}
+
+	public void setGrade() {
+		
+		int totalmarks = Arrays.stream(marks).mapToInt(Integer::intValue).sum();
+		if(totalmarks>=240 && totalmarks<=300)
+			grade="Excellent";
+		if(totalmarks>=180 && totalmarks<=239)
+			grade="Good";
+		if(totalmarks>=150 && totalmarks<=179)
+			grade="Average";
+		if(totalmarks<150)
+			grade="Fail";
+		
+		this.grade = grade;
+	}
+
+	public static Integer getAdmissionCounter() {
+		return admissionCounter;
+	}
+
+	public static void setAdmissionCounter(Integer admissionCounter) {
+		Student.admissionCounter = admissionCounter;
+	}
+
+	public static String getSchoolName() {
+		return SchoolName;
+	}
+
+	public static void setSchoolName(String schoolName) {
+		SchoolName = schoolName;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [admissionCode=" + admissionCode + ", studentName=" + studentName + ", birthdate=" + birthdate
+				+ ", marks=" + Arrays.toString(marks) + ", grade=" + grade + "]";
+	}
+}
---
+++ EY Assignment Day4/StudentTester.java
@@ -0,0 +1,26 @@
+package com.ey.oops.task;
+import java.util.Date;
+
+public class StudentTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Student.setAdmissionCounter(100);
+		Student students[]=new Student[5];
+		Integer a[]= {45,42,67};
+		students[0]=new Student("ramesh1",new Date(103,4,6),a);
+		students[0].setGrade();
+		students[1]=new Student("ramesh2",new Date(104,5,8),a);
+		students[1].setGrade();
+		students[2]=new Student("ramesh3",new Date(102,1,2),a);
+		students[2].setGrade();
+		students[3]=new Student("ramesh4",new Date(107,3,4),a);
+		students[3].setGrade();
+		students[4]=new Student("ramesh5",new Date(108,3,4),a);
+		students[4].setGrade();
+		StudentService sc=new StudentService();
+		sc.ShowStudentDetails(students);
+	}
+
+}
